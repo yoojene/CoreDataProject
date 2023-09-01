402aef6113b379af18b8ea65243216e789b0beb4
Eugene Cross <eugene.cross@gmail.com>
2023-09-01 16:37:51 +0100

    Setup DataController and CoreData model

---
+++ CoreDataProject/DataController.swift
@@ -0,0 +1,21 @@
+//
+//  DataController.swift
+//  Bookworm
+//
+//  Created by Eugene on 01/09/2023.
+//
+
+import CoreData
+import Foundation
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "CoreDataProject")
+    
+    init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Core Data failed to load: \(error.localizedDescription)")
+            }
+        }
+    }
+}
