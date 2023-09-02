da8fc5d31aa31b71c5f9a78375a8559d397a9fb6
Eugene Cross <eugene.cross@gmail.com>
2023-09-02 12:16:22 +0100

    Reset to Singer FilteredList for challenges

--- CoreDataProject/FilteredList.swift
+++ CoreDataProject/FilteredList.swift
@@ -8,21 +8,17 @@
 import SwiftUI
 import CoreData
 
-struct FilteredList<T: NSManagedObject, Content: View>: View {
-    @FetchRequest var fetchRequest: FetchedResults<T>
-
-    let content: (T) -> Content
+struct FilteredList: View {
     
+    @FetchRequest var fetchRequest: FetchedResults<Singer>
     var body: some View {
         List(fetchRequest, id: \.self) { singer in
-            self.content(singer)
+            Text("\(singer.wrappedFirstName) \(singer.wrappedLastName)")
         }
     }
     
-    init(filterKey: String, filterValue: String, @ViewBuilder content: @escaping (T) -> Content) {
-        
-        _fetchRequest = FetchRequest<T>(sortDescriptors: [], predicate: NSPredicate(format: "%K BEGINSWITH %@", filterKey, filterValue))
-        self.content = content
+    init(filter: String) {
+        _fetchRequest = FetchRequest<Singer>(sortDescriptors: [], predicate: NSPredicate(format: "lastName BEGINSWITH %@", filter))
     }
 }
 
